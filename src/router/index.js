9f5d9a21b201f163b22b819778f27a55a9ad52de
Tanin <ber_zebub@hotmail.com>
2021-03-06 20:39:54 +0700

    Connect to DB

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
-import { route } from 'quasar/wrappers'
-import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
-import routes from './routes'
+import { route } from "quasar/wrappers";
+import {
+  createRouter,
+  createMemoryHistory,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
+import routes from "./routes";
 
 /*
  * If not building with SSR mode, you can
@@ -11,10 +16,28 @@ import routes from './routes'
  * with the Router instance.
  */
 
+var firebaseConfig = {
+  apiKey: "AIzaSyBIcNGlkX1u3cQVM9DPk12R_UL16KsCfwk",
+  authDomain: "bhms-5e304.firebaseapp.com",
+  projectId: "bhms-5e304",
+  storageBucket: "bhms-5e304.appspot.com",
+  messagingSenderId: "457829213069",
+  appId: "1:457829213069:web:85f0d96a6025e6a7efd3c4",
+  measurementId: "G-J3H2KGXGWZ",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const db = firebase.firestore()
+
+
 export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
     ? createMemoryHistory
-    : process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory
+    : process.env.VUE_ROUTER_MODE === "history"
+    ? createWebHistory
+    : createWebHashHistory;
 
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -23,8 +46,10 @@ export default route(function (/* { store, ssrContext } */) {
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE)
-  })
+    history: createHistory(
+      process.env.MODE === "ssr" ? void 0 : process.env.VUE_ROUTER_BASE
+    ),
+  });
 
-  return Router
-})
+  return Router;
+});
